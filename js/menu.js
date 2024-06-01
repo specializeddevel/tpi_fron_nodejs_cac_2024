db55db7f5657141af4e29bddb5ed9f6df17fdf42
Raul Burgos Murray <specializeddevel@gmail.com>
2024-06-01 20:48:46 -0300

    Updated menu.js with close menu function

--- js/menu.js
+++ js/menu.js
@@ -8,6 +8,13 @@
     const openButton = document.querySelector('.nav__menu');
     const menu = document.querySelector('.nav__link');
     const closeMenu = document.querySelector('.nav__close');
+    const menuLinks = document.querySelectorAll('.nav__link a');
+
+    menuLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            menu.classList.remove('nav__link--show');
+        });
+    });
 
     openButton.addEventListener('click', ()=>{
         menu.classList.add('nav__link--show');
